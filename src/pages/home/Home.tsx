deac8ee1a0bd6546382e982dd91eb61104865ac3
swbonjour <71038541+swbonjour@users.noreply.github.com>
2022-09-26 04:05:20 +0300

    links

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from "react"
 
 import './Home.scss'
 
@@ -8,24 +9,46 @@ import { WhatsApp } from 'src/img/whatsApp'
 import { Telegram } from 'src/img/telegram'
 
 export function Home() {
-  return (
-    <main className="home">
-      <div className="home__content">
-        <Header></Header>
-        <div className="home__content_text">
-        <Text text="Маректинговое агенство. С нами вы растете как на дрожжах"></Text>
-        </div>
-        <div className="home__contact">
-          <div className="home__contact_item">
-            <WhatsApp></WhatsApp>
-            <a href="#" className="home__contact_item_text"><Text text="WHATSAPP" color='gray'></Text></a>
+  const [width, setWidth] = useState(document.documentElement.clientWidth)
+
+  window.addEventListener('resize', () => {
+    setWidth(document.documentElement.clientWidth)
+  })
+  if(width > 320) {
+    return (
+      <main className="home">
+        <div className="home__content">
+          <Header></Header>
+          <div className="home__content_text">
+          <Text text="Маректинговое агенство. С нами вы растете как на дрожжах"></Text>
           </div>
-          <div className="home__contact_item">
-            <Telegram></Telegram>
-            <a href="#" className="home__contact_item_text"><Text text="TELEGRAM" color='gray'></Text></a>
+          <div className="home__contact">
+            <div className="home__contact_item">
+              <WhatsApp></WhatsApp>
+              <a href="#" className="home__contact_item_text"><Text text="WHATSAPP" color='gray'></Text></a>
+            </div>
+            <div className="home__contact_item">
+              <Telegram></Telegram>
+              <a href="#" className="home__contact_item_text"><Text text="TELEGRAM" color='gray'></Text></a>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
-  )
+      </main>
+    )
+  } else {
+    return (
+      <main className="home">
+        <div className="home__content">
+          <Header></Header>
+          <div className="home__content_text">
+          <Text text="Маректинговое агенство. С нами вы растете как на дрожжах"></Text>
+          </div>
+          <div className="home__contact">
+              <WhatsApp></WhatsApp>
+              <Telegram></Telegram>
+          </div>
+        </div>
+      </main>
+    )
+  }
 }
